7fcfc186863db2f22d2c3eb08839b892b681c3f3
Mehdi <mhdfueradecontrol@gmail.com>
2026-02-05 00:05:00 +0100

    Mehdi work

---
+++ algorithms.py
@@ -0,0 +1,49 @@
+from graph_model import GraphModeling
+
+class GraphAlgorithms(GraphModeling):
+    def bfs(self, start_node):
+        """
+        Explore la connectivité pour trouver toutes les pages accessibles.
+        Utile pour identifier les 'Pages Isolées' (Personne 4).
+        """
+        visited = set()
+        queue = [start_node]
+        visited.add(start_node)
+        
+        while queue:
+            current = queue.pop(0)
+            for neighbor in self.get_successors(current):
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+        return visited
+
+    def has_cycle(self):
+        """
+        Détecte les boucles de navigation (Cycles).
+        Objectif clé du projet pour l'étude de la structure interne.
+        """
+        visited = set()
+        rec_stack = set() # Pile de récursion pour suivre le chemin actuel
+
+        def dfs_visit(node):
+            visited.add(node)
+            rec_stack.add(node)
+
+            for neighbor in self.get_successors(node):
+                if neighbor not in visited:
+                    if dfs_visit(neighbor):
+                        return True
+                elif neighbor in rec_stack:
+                    # Un cycle est détecté ici
+                    return True
+
+            rec_stack.remove(node)
+            return False
+
+        # On vérifie chaque nœud pour s'assurer de couvrir tout le graphe
+        for node in self.nodes_info:
+            if node not in visited:
+                if dfs_visit(node):
+                    return True
+        return False
